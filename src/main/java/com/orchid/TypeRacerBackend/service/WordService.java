7d88900a5d0217cac74a7aaf7caef3f5161d801b
Mihai Latea <latea.alexandru@yahoo.com>
2022-10-14 11:10:17 +0300

    [TYP-11] Created Word Service

--- src/main/java/com/orchid/TypeRacerBackend/service/WordService.java
+++ src/main/java/com/orchid/TypeRacerBackend/service/WordService.java
@@ -1,2 +1,22 @@
-package com.orchid.TypeRacerBackend.service;public class WordService {
+package com.orchid.TypeRacerBackend.service;
+
+import com.orchid.TypeRacerBackend.entity.Word;
+import com.orchid.TypeRacerBackend.repository.WordRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class WordService {
+
+    @Autowired
+    private WordRepository wordRepository;
+
+    public List<Word> getWords(){
+        return wordRepository.findAll();
+    }
+
 }
