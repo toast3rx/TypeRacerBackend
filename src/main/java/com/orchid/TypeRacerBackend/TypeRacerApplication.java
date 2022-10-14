66a6e1a3b4a74dd93abf17cf27778b7ae67c8b0a
Mihai Latea <latea.alexandru@yahoo.com>
2022-10-14 09:29:12 +0300

    first commit

---
+++ src/main/java/com/orchid/TypeRacerBackend/TypeRacerApplication.java
@@ -0,0 +1,13 @@
+package com.orchid.TypeRacerBackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TypeRacerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TypeRacerApplication.class, args);
+	}
+
+}
